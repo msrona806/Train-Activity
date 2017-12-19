7ac4352149a5fd778daf572e19d2623e4419e2f0
Sharona Young <youngs_e@hotmail.com>
2017-12-18 23:08:42 -0500

    added firebase link

---
+++ assets/javascript/train.js
@@ -0,0 +1,13 @@
+
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDbq0Z4XLhY1Wy3axqEHKOVeJDarBJTUUc",
+    authDomain: "train-schedules-db156.firebaseapp.com",
+    databaseURL: "https://train-schedules-db156.firebaseio.com",
+    projectId: "train-schedules-db156",
+    storageBucket: "",
+    messagingSenderId: "1055790770682"
+  };
+  firebase.initializeApp(config);
+</script>
